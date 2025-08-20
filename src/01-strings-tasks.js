32737e999eedbe2b158f580735d8ea9d693d662c
YekaterinaAlex <ekaterina.alekseyenko@gmail.com>
2025-08-20 12:42:25 -0700

    fix string

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -276,7 +276,30 @@ function isString(value) {
  *   'K♠' => 51
  */
 function getCardId(value) {
-  return;
+  const ranks = [
+    'A',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+  ];
+  const suits = ['♣', '♦', '♥', '♠'];
+
+  const rank = value.slice(0, -1);
+  const suit = value.slice(-1);
+
+  const rankIndex = ranks.indexOf(rank);
+  const suitIndex = suits.indexOf(suit);
+
+  return suitIndex * ranks.length + rankIndex;
 }
 
 module.exports = {
